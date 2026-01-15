7c9804369f53a676014667894b87713810d99212
shoebilyas123 <shoebilyas123@gmail.com>
2026-01-15 14:23:23 +0530

    feat: use ioctl to get window size and fallback to OS if ioctl fails

--- scribe.c
+++ scribe.c
@@ -6,18 +6,25 @@
 #include <sys/termios.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 // Macros
 #define CTRL_KEY(k)((k) & 0x1f)
 
 /** Data Structures */
-struct termios orig_term;
+struct editorconf {
+  struct termios orig_term;
+  unsigned short scrnrows;
+  unsigned short scrncols;
+};
+
+struct editorconf E;
 
 
 /** Terminal Manipulation */
 void drawrows() {
   int y = 0;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < E.scrncols; y++) {
       write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
@@ -38,21 +45,21 @@ void die(const char *s) {
 }
 
 void disable_raw_mode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term)==-1) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_term)==-1) {
       die("tcsetattr");
     }
 }
 
 void enable_raw_mode() {
   // We are fetching the terminal attributes at start
-  if (tcgetattr(STDIN_FILENO, &orig_term) == -1) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_term) == -1) {
     die("tcgetattr");
  }
   // This function is called when our program exits
   atexit(disable_raw_mode);
 
   // The copying the struct value to tc_attr;
-  struct termios tc_attr = orig_term;
+  struct termios tc_attr = E.orig_term;
   // Setting the ECHO flag to zero using bitwise operation
 
   tc_attr.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
@@ -77,6 +84,42 @@ char editor_read_key() {
   return c;
 }
 
+int getCursorPos(unsigned short *rows, unsigned short *cols) {
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) {
+    return -1;
+  }
+
+  char buf[32];
+  unsigned int i = 0;
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      return -1;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 1) return -1;
+  return 0;
+}
+
+int getWindowSize(unsigned short *rows, unsigned short *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPos(rows, cols);
+  }
+
+  *cols = ws.ws_col;
+  *rows = ws.ws_row;
+  return 0;
+}
+
 /** Input Handling */
 
 void process_key_press() {
@@ -92,12 +135,18 @@ void process_key_press() {
     }
 }
 
-/** Main */
+/** Init Editor */
+
+void setup_editor() {
+  if (getWindowSize(&E.scrnrows, &E.scrncols) == -1) die("getWindowSize");
+}
+
 int main() {
+    setup_editor();
     enable_raw_mode();
     while (1) {
       clearscrn();
-        process_key_press();
+      process_key_press();
     }
     return 0;
 }
