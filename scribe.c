cb1a532c98b3663f2f068728573dcc212805fcc3
shoebilyas123 <shoebilyas123@gmail.com>
2026-02-07 18:53:41 +0530

    feat: added cursor movements and file io

--- scribe.c
+++ scribe.c
@@ -1,7 +1,7 @@
-
 #include <errno.h>
 #include <ctype.h>
 #include <stdatomic.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,18 +9,30 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
 
 // Macros
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
 
 /** Data Structures */
+typedef struct rows {
+  char *line;
+  int len;
+} erow;
+
 struct editorconf {
   int cx;
   int cy;
   struct termios orig_term;
   unsigned short scrnrows;
   unsigned short scrncols;
+  erow row;
+  int numrows;
 };
 
 struct editorconf E;
@@ -29,6 +41,18 @@ struct editorconf E;
 struct abuf {
   char *b;
   int len;
+};
+
+enum CRS_DIR {
+  ARROW_UP = 1000,
+  ARROW_DOWN,
+  ARROW_RIGHT,
+  ARROW_LEFT,
+  PAGE_UP,
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY,
+  DEL_KEY,
 };
 
 #define ABUF_INIT {NULL, 0}
@@ -51,7 +75,8 @@ void abFree(struct abuf *ab) {
 void drawrows(struct abuf *ab) {
   int y = 0;
   for (y = 0; y < E.scrnrows; y++) {
-      if (y == E.scrnrows / 3) {
+    if (y >= E.numrows) {
+      if (y == E.scrncols / 3) {
           char welcome[80];
           int welcomelen = snprintf(welcome, sizeof(welcome),
             "Scribe editor -- version %s", KILO_VERSION);
@@ -71,7 +96,11 @@ void drawrows(struct abuf *ab) {
         } else {
           abAppend(ab, "~", 1);
         };
-
+    } else {
+      int max_length = E.row.len;
+      if(max_length > E.scrncols) max_length = E.scrncols;
+        abAppend(ab, E.row.line, max_length);
+    }
     // This commands the terminal to clear one line at a time
     abAppend(ab, "\x1b[K", 3);
     if (y < E.scrnrows - 1) {
@@ -130,7 +159,7 @@ void enable_raw_mode() {
   }
 }
 
-char editor_read_key() {
+int editor_read_key() {
   int bytesread;
   char c;
 
@@ -138,22 +167,76 @@ char editor_read_key() {
       if(bytesread == -1 && errno != EAGAIN) die("read");
   }
 
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+      return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+      return '\x1b';
+
+    if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+              case '1': return HOME_KEY;
+              case '7': return HOME_KEY;
+              case '4': return END_KEY;
+              case '8': return END_KEY;
+              case '5': return PAGE_UP;
+              case '6':
+                return PAGE_DOWN;
+                case '3': return DEL_KEY;
+
+          }
+        }
+      } else {
+        switch (seq[1]) {
+            case 'A':  return ARROW_UP;
+            case 'B':  return ARROW_DOWN;
+            case 'C':  return ARROW_RIGHT;
+            case 'D':
+              return ARROW_LEFT;
+            case 'H': return HOME_KEY;
+                       case 'F': return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
+          switch (seq[1]) {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
+          }
+    }
+
+    return '\x1b';
+  } else {
   return c;
+  }
 }
 
-void editor_mv_crsr(char key) {
+void editor_mv_crsr(int key) {
   switch (key) {
-  case 'a':
+  case ARROW_LEFT:
+    if (E.cx - 1 >= 0) {
     E.cx--;
+  }
     break;
-  case 'd':
-    E.cx++;
+  case ARROW_RIGHT:
+  if (E.cx + 1 < E.scrncols) {
+  E.cx++;
+}
     break;
-    case 'w':
-      E.cy--;
+    case ARROW_UP:
+    if (E.cy - 1 >= 0) {
+    E.cy--;
+  }
       break;
-      case 's':
-        E.cy++;
+      case ARROW_DOWN:
+      if (E.cy + 1 < E.scrnrows) {
+      E.cy++;
+    }
         break;
   }
 }
@@ -196,17 +279,30 @@ int getWindowSize(unsigned short *rows, unsigned short *cols) {
 /** Input Handling */
 
 void process_key_press() {
-  char c = editor_read_key();
+  int c = editor_read_key();
 
   switch (c) {
   case CTRL_KEY('q'):
     clearscrn();
     exit(0);
+    break;
+    case HOME_KEY:
+        E.cx = 0;
         break;
-    case 'a':
-    case 'd':
-    case 'w':
-    case 's':
+      case END_KEY:
+        E.cx = E.scrncols - 1;
+        break;
+
+  case PAGE_UP:
+    E.cy = 0;
+    break;
+  case PAGE_DOWN:
+    E.cy = E.scrnrows - 1;
+    break;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_RIGHT:
+    case ARROW_LEFT:
     editor_mv_crsr(c);
     break;
     default:
@@ -214,16 +310,43 @@ void process_key_press() {
     }
 }
 
+/** File I/O */
+void open_editor(char *f_path) {
+  FILE *fp = fopen(f_path, "r");
+  size_t linecap = 0;
+  char *line = NULL;
+  ssize_t size = getline(&line, &linecap, fp);
+  if (size != -1) {
+    while (size > 0 && (line[size - 1] == '\n' || line[size - 1] == '\r'))
+      size--;
+    E.row.len = size;
+    E.row.line = malloc(size+1);
+    memcpy(E.row.line, line, size);
+    E.row.line[size] = '\0';
+    E.numrows += 1;
+
+  }
+
+  free(line);
+  fclose(fp);
+}
+
 /** Init Editor */
 
 void setup_editor() {
+  E.cx = 0;
+  E.cy = 0;
+  E.numrows = 0;
   if (getWindowSize(&E.scrnrows, &E.scrncols) == -1) die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     setup_editor();
     enable_raw_mode();
 
+  if (argc >= 1) {
+      open_editor(argv[1]);
+  }
     while (1) {
         clearscrn();
       process_key_press();
